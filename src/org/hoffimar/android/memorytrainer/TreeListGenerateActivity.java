0ab68cdaff959f86e18b32c07175f7d24c0916d0
hoffimar <hoffimar@gmail.com>
2010-08-16 18:55:41 +0200

    Added chinese translation by http://goapk.com

--- src/org/hoffimar/android/memorytrainer/TreeListGenerateActivity.java
+++ src/org/hoffimar/android/memorytrainer/TreeListGenerateActivity.java
@@ -3,7 +3,6 @@ package org.hoffimar.android.memorytrainer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
